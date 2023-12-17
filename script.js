eebc63eeab96a3152a85a7a6dd4cd6e0cf733ba7
Hamza <59485770+Crepco@users.noreply.github.com>
2023-12-17 14:20:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Sample blog post data
     const posts = [
-        { title: 'First Post', content: 'This is the content of the first post.' },
-        { title: 'Second Post', content: 'This is the content of the second post.' },
+        { title: 'Post Coming soon!', content: 'TComing Soon' },
         // Add more posts as needed
     ];
 
